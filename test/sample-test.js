3be11ce04cde50d7f6c5dbcade3f843c5a570d07
Rahul <rahulcy7@gmail.com>
2022-06-14 06:57:41 +0530

    initialize basic web3 dapp

---
+++ test/sample-test.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MyNFT", function () {
+  it("Should mint and transfer an NFT to someone", async function () {});
+});
